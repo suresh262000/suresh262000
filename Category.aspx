ab2040c2807211582f5c7b636f3fc26c38e84171
suresh262000 <139310299+suresh262000@users.noreply.github.com>
2023-12-27 16:52:48 +0530

    Add files via upload

---
+++ Category.aspx
@@ -0,0 +1,174 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout.Master" AutoEventWireup="true" CodeBehind="Category.aspx.cs" Inherits="HRMS_.Category" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+     <style>
+        .h3, h3 {
+            font-size: 1.5rem;
+        }
+        .fa {
+            padding-right: 0px;
+        }
+    </style>
+    <script>
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    </script>
+    <script>
+        function createModal() {
+            $('#createcategorymodal').modal('show');
+        }
+    </script>
+    
+    <script>
+        function editModal() {
+            $('#editcategorymodal').modal('show');
+        }
+    </script>
+    <script>
+        $(document).ready(function () {
+
+            $('#smartwizard').smartWizard({
+                selected: 0,
+                theme: 'dots',
+                autoAdjustHeight: true,
+                transitionEffect: 'fade',
+                showStepURLhash: false,
+
+            });
+
+        });
+    </script>
+    <script>
+        var object = { status: false, ele: null };
+        function confirmdelete(ev) {
+
+
+            if (object.status) { return true; };
+
+            swal({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: '#e91d1d',
+                cancelButtonColor: '#2a7de5',
+                confirmButtonText: "Yes",
+                cancelButtonText: "Cancel",
+                closeOnConfirm: true
+            },
+                function () {
+                    object.status = true;
+                    object.ele = ev;
+                    object.ele.click();
+                });
+
+            return false;
+        };
+
+    </script>
+    
+    <div class="main_container">
+        <h2 class="header">
+            <i class="fa fa-code-fork"></i>
+            Category
+        </h2>
+
+    </div>
+    <br />
+    <asp:Button ID="addcategory" CssClass="btn button-1" OnClick="addcategory_Click" runat="server" Text="+ Add Category" />
+    <asp:Button ID="downloadcategory" CssClass="pull-right btn button-1" OnClick="downloadcategory_Click" runat="server"  Text="Download " />
+    <br />
+    <br />
+    <asp:GridView ID="categorygrid" runat="server" OnRowDeleting="categorygrid_RowDeleting" AutoGenerateColumns="False" CssClass="table table-striped  dt-responsive nowrap datatable1" width="100%" DataKeyNames="Category_ID" EmptyDataText="No Data Available" ShowHeaderWhenEmpty="True" OnRowEditing="categorygrid_RowEditing" CellPadding="4" ForeColor="#333333" GridLines="None">
+        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        <Columns>
+            <asp:TemplateField HeaderText="Category Name"><ItemTemplate>
+                    <asp:Label ID="categoryname" runat="server" Text='<%# Bind("Category_Name") %>'></asp:Label>
+                </ItemTemplate></asp:TemplateField>
+            <asp:TemplateField HeaderText="Category Description"><ItemTemplate>
+                    <asp:Label ID="categorydesc" runat="server" Text='<%# Bind("Category_Desc") %>'></asp:Label>
+                </ItemTemplate></asp:TemplateField>
+            <asp:TemplateField HeaderText="Edit"> <ItemTemplate >
+                                        <asp:LinkButton ID="cEdit" runat="server" OnCommand="cEdit_Command" CommandArgument='<%# Eval("Category_ID") %>' CommandName="Edit">
+                    <i class="fa fa-edit"></i>
+                    </asp:LinkButton>
+                </ItemTemplate></asp:TemplateField>
+            <asp:TemplateField HeaderText="Delete"> <ItemTemplate >
+                                        <asp:LinkButton ID="cDelete" runat="server" CommandName="Delete"
+                        OnClientClick="return confirmdelete(this);">
+                    <i class="fa fa-trash"></i>
+                    </asp:LinkButton>
+                </ItemTemplate></asp:TemplateField>
+        </Columns>
+        <EditRowStyle BackColor="#999999" />
+        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+    </asp:GridView>
+
+       
+    <!-- To Create Category Modol Popup -->
+
+
+    <div class="container">
+        <div class="modal fade " data-backdrop="false" id="createcategorymodal" role="dialog">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">ADD CATEGORY</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <label>Category Name</label>
+                        <asp:TextBox ID="categoryname" CssClass="form-control" placeholder="Category Name" runat="server" ></asp:TextBox>
+                        <asp:Label ID="catname" runat="server"  ForeColor="Red"></asp:Label>
+                        <br /> <label>Category Description</label>
+                        <asp:TextBox ID="categorydesc" CssClass="form-control" placeholder="Category Description" runat="server" ></asp:TextBox>
+                        <asp:Label ID="catdesc" runat="server"  ForeColor="Red"></asp:Label>
+                     
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <asp:Button ID="btnSave" CssClass="btn btn-primary" OnClick="btnSave_Click" Text="Save" runat="server" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--    To Edit Category Modal Popup -->
+
+    <div class="container">
+        <div class="modal fade " data-backdrop="false" id="editcategorymodal" role="dialog">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">EDIT CATEGORY</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        
+                        <label>Category Name</label>
+                        <asp:TextBox ID="ecategoryname" CssClass="form-control" placeholder="Category Name" runat="server" ></asp:TextBox>
+                        <asp:Label ForeColor="Red" ID="ecatname" runat="server" ></asp:Label>
+                        <br /><label>Category Description</label>
+                        <asp:TextBox Enabled="true" ID="ecategorydesc" CssClass="form-control" placeholder="Category Description" runat="server"  ></asp:TextBox>
+                       <asp:Label ForeColor="Red" ID="ecatdesc" runat="server"  ></asp:Label>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <asp:Button ID="update" OnClick="update_Click1" CssClass="btn btn-primary" Text="Update" runat="server"  />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
+
---
+++ Category.aspx.cs
@@ -0,0 +1,391 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HRMS_
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["HRMSDB"].ConnectionString;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                loadCategory();
+            }
+
+        }
+        protected void loadCategory()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * From Category", con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+                categorygrid.DataSource = ds;
+                categorygrid.DataBind();
+            }
+            if (categorygrid.FooterRow != null)
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+                categorygrid.FooterRow.TableSection = TableRowSection.TableFooter;
+            }
+            else
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
+        }
+        protected void clearCreateDep()
+        {
+            catname.Text = string.Empty;
+
+            catdesc.Text = string.Empty;
+           
+
+        }
+
+        
+
+
+        protected void cleareditDep()
+        {
+            ecategoryname.Text = string.Empty;
+            ecategorydesc.Text = string.Empty;
+            
+
+        }
+        // to save data into database
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+
+            if (categorygrid.FooterRow != null)
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+                categorygrid.FooterRow.TableSection = TableRowSection.TableFooter;
+            }
+            else
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
+            clearcreateError();
+            try
+            {
+
+                if (categoryname.Text == string.Empty)
+                {
+                    catname.Text = "Please Enter Category Name";
+                    ClientScript.RegisterStartupScript(this.GetType(), "Pop", "createModal();", true);
+                    return;
+                }
+
+               
+
+                else if (categorydesc.Text == string.Empty)
+                {
+                    catdesc.Text = "Please Enter Category Description";
+                    ClientScript.RegisterStartupScript(this.GetType(), "Pop", "createModal();", true);
+                    return;
+
+                }
+
+                
+                else
+                {
+                    clearcreateError();
+                }
+
+                SqlConnection con = new SqlConnection(connectionString);
+
+                con.Open();
+                DateTime nowDateTime = DateTime.Now;
+                string un = Session["username"].ToString();
+                string formattedDateTime = nowDateTime.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+                SqlCommand cmd = new SqlCommand("Insert into Category (Category_Name,Category_Desc,IsActive,Created_By,Created_Date) Values" +
+                 " ('" + categoryname.Text + "','" + categorydesc.Text + "','" + 1 + "','" + un + "','" + nowDateTime + "')", con);
+                int i = cmd.ExecuteNonQuery();
+                loadCategory();
+                clearCreateDep();
+                con.Close();
+                if (i > 0)
+                {
+                    ClientScript.RegisterClientScriptBlock
+                   (this.GetType(), "K", "swal('Created','Category Added Successfully!','success')", true);
+                }
+                clearCreateDep();
+            }
+            catch (Exception ex)
+            {
+
+                ClientScript.RegisterClientScriptBlock
+                   (this.GetType(), "K", "swal('Error','Error Occured While Creating!','error')", true);
+            }
+
+        }
+
+        // to delete data into database
+        protected void categorygrid_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+            if (categorygrid.FooterRow != null)
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+                categorygrid.FooterRow.TableSection = TableRowSection.TableFooter;
+            }
+            else
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
+            try
+            {
+
+
+                SqlConnection con = new SqlConnection(connectionString);
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Delete from Category where Category_ID=@catid ", con);
+                cmd.Parameters.AddWithValue("@catid", categorygrid.DataKeys[e.RowIndex].Value.ToString());
+
+
+                int r = cmd.ExecuteNonQuery();
+                loadCategory();
+                con.Close();
+                if (r > 0)
+
+                {
+
+                    ClientScript.RegisterClientScriptBlock
+                    (this.GetType(), "K", "swal('Deleted!','Category has been deleted!','success')", true);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ClientScript.RegisterClientScriptBlock
+                   (this.GetType(), "K", "swal('Foreign Key Constraint Violation','Cannot Delete Record.Associated Data In Use.','error')", true);
+            }
+
+        }
+
+
+        protected void downloadcategory_Click(object sender, EventArgs e)
+        {
+            DataTable dtexcel = new DataTable();
+            // GridView gv = new GridView();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+
+                con.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter("Select Category_ID as [Category ID], Category_Name as [Category Name],Category_Desc as [Category Description],Created_By as [Created By],Created_Date as [Created Date],Modified_By as [Modified By],Modified_Date as [Modified Date] from Category", con);
+                da.Fill(dtexcel);
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+                    wb.Worksheets.Add(dtexcel, "Category");
+
+                    Response.Clear();
+                    Response.Buffer = true;
+                    Response.Charset = "";
+                    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    Response.AddHeader("content-disposition", "attachment;filename=Category Details.xlsx");
+                    using (MemoryStream MyMemoryStream = new MemoryStream())
+                    {
+                        wb.SaveAs(MyMemoryStream);
+                        MyMemoryStream.WriteTo(Response.OutputStream);
+                        Response.Flush();
+                        Response.End();
+                    }
+                }
+
+            }
+
+
+        }
+
+       
+        private void cleareditError()
+        {
+            ecatname.Text ="";
+            ecatdesc.Text ="";
+                                 
+
+        }
+        private void clearcreateError()
+        {
+           catname.Text ="";
+            catdesc.Text = "";
+        }
+
+
+
+
+
+        protected void cEdit_Command(object sender, CommandEventArgs e)
+        {
+
+
+            string cid = e.CommandArgument.ToString();
+
+            SqlConnection con1 = new SqlConnection(connectionString);
+
+
+            con1.Open();
+
+
+            SqlCommand cmd1 = new SqlCommand("Select * from Category where Category_ID=@cid", con1);
+            cmd1.Parameters.AddWithValue("@cid", cid);
+            SqlDataReader dr = cmd1.ExecuteReader();
+
+            while (dr.Read())
+            {
+               
+                ecategoryname.Text = dr["Category_Name"].ToString();
+                ecategorydesc.Text = dr["Category_Desc"].ToString();
+                
+
+            }
+            dr.Close();
+
+
+        }
+
+        
+
+
+        
+
+        protected void categorygrid_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+            string script = "$('#editcategorymodal').modal('show');";
+            ClientScript.RegisterStartupScript(this.GetType(), "Popupedit", script, true);
+            if (categorygrid.FooterRow != null)
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+                categorygrid.FooterRow.TableSection = TableRowSection.TableFooter;
+            }
+            else
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
+            
+        }
+
+        protected void update_Click1(object sender, EventArgs e)
+        {
+
+           
+            if (categorygrid.FooterRow != null)
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+                categorygrid.FooterRow.TableSection = TableRowSection.TableFooter;
+            }
+            else
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
+            try
+            {
+                if (ecategoryname.Text == string.Empty)
+                {
+                    ecatname.Text = "Please Enter Category Name";
+                    ClientScript.RegisterStartupScript(this.GetType(), "Pop", "editModal();", true);
+                    return;
+                }
+
+                else if (ecategorydesc.Text == string.Empty)
+                {
+                    ecatname.Text=string.Empty; 
+                    ecatdesc.Text = "Please Enter Category Description";
+                    ClientScript.RegisterStartupScript(this.GetType(), "Pop", "editModal();", true);
+                    return;
+
+                }
+
+
+                else
+                {
+                    cleareditError();
+                                       
+                }
+                    SqlConnection con = new SqlConnection(connectionString);
+                    con.Open();
+                    DateTime nowDateTime = DateTime.Now;
+                    string un = Session["username"].ToString();
+                    SqlCommand cmd = new SqlCommand("Update Category Set Category_Name=@catname,Category_Desc=@catdesc,IsActive=@catisActive,Modified_By=@catmodifiedby,Modified_Date=@catmodifieddate where Category_ID=@id ", con);
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(categorygrid.DataKeys[categorygrid.EditIndex].Value));
+                    cmd.Parameters.AddWithValue("@catname", ecategoryname.Text);
+                    cmd.Parameters.AddWithValue("@catdesc", ecategorydesc.Text);
+                    cmd.Parameters.AddWithValue("@catisActive", 1);
+                    cmd.Parameters.AddWithValue("@catmodifiedby", un);
+                    cmd.Parameters.AddWithValue("@catmodifieddate", nowDateTime);
+                    int r = cmd.ExecuteNonQuery();
+                  
+
+                
+                loadCategory();
+                    cleareditDep();
+                    con.Close();
+                    if (r > 0)
+                    {
+                        ClientScript.RegisterClientScriptBlock
+                        (this.GetType(), "K", "swal('Updated!','Category has been Updated !','success')", true);
+                    }
+                
+            }
+            catch (Exception ex)
+            {
+
+                ClientScript.RegisterClientScriptBlock
+                   (this.GetType(), "K", "swal('Error','Error Occured While Updating!','error')", true);
+            }
+            
+
+        }
+
+        protected void addcategory_Click(object sender, EventArgs e)
+        {
+            string script = "$('#createcategorymodal').modal('show');";
+            ClientScript.RegisterStartupScript(this.GetType(), "Popupedit", script, true);
+            if (categorygrid.FooterRow != null)
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+                categorygrid.FooterRow.TableSection = TableRowSection.TableFooter;
+            }
+            else
+            {
+                categorygrid.UseAccessibleHeader = true;
+                categorygrid.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
+        }
+
+        
+
+        
+    }
+}
